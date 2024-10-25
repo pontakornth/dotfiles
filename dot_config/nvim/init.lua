b81f76df765cec72a410a794460063b9ecf709f5
pontakornth <pontakorn61@gmail.com>
2024-10-25 12:54:21 +0700

    feat: Change to custom config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,9 @@
+require("config.lazy")
+
+-- Mini configuration
+require('mini.basics').setup()
+require('mini.starter').setup()
+require('mini.surround').setup()
+require('mini.icons').setup()
+require('mini.statusline').setup()
+
