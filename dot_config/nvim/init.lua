efa9ed278a2c0601225cd62e9c8e29ae9a37a1cb
pontakornth <pontakorn61@gmail.com>
2024-11-03 20:12:49 +0700

    feat: Install more plugins

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,91 +1,205 @@
 -- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-local path_package = vim.fn.stdpath('data') .. '/site/'
-local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+local path_package = vim.fn.stdpath("data") .. "/site/"
+local mini_path = path_package .. "pack/deps/start/mini.nvim"
 if not vim.loop.fs_stat(mini_path) then
-  vim.cmd('echo "Installing `mini.nvim`" | redraw')
-  local clone_cmd = {
-    'git', 'clone', '--filter=blob:none',
-    'https://github.com/echasnovski/mini.nvim', mini_path
-  }
-  vim.fn.system(clone_cmd)
-  vim.cmd('packadd mini.nvim | helptags ALL')
-  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+	vim.cmd('echo "Installing `mini.nvim`" | redraw')
+	local clone_cmd = {
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/echasnovski/mini.nvim",
+		mini_path,
+	}
+	vim.fn.system(clone_cmd)
+	vim.cmd("packadd mini.nvim | helptags ALL")
+	vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
 -- Set up 'mini.deps' (customize to your liking)
-require('mini.deps').setup({ path = { package = path_package } })
+require("mini.deps").setup({ path = { package = path_package } })
+
+-- Configuration
+vim.opt.signcolumn = "yes"
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
 -- Install plugins
 
 local add = MiniDeps.add
 add({
-  source = 'neovim/nvim-lspconfig',
-  -- Supply dependencies near target plugin
-  depends = { 'williamboman/mason.nvim' },
+	source = "neovim/nvim-lspconfig",
+	-- Supply dependencies near target plugin
+	depends = { "williamboman/mason.nvim" },
+})
+add({ source = "VonHeikemen/lsp-zero.nvim", checkout = "v4.x" })
+add({
+	source = "hrsh7th/nvim-cmp",
+	depends = {
+		"hrsh7th/cmp-nvim-lsp",
+	},
 })
 
 add({
-  source = 'nvim-treesitter/nvim-treesitter',
-  -- Use 'master' while monitoring updates in 'main'
-  checkout = 'master',
-  monitor = 'main',
-  -- Perform action after every checkout
-  hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+	source = "nvim-treesitter/nvim-treesitter",
+	-- Use 'master' while monitoring updates in 'main'
+	checkout = "master",
+	monitor = "main",
+	-- Perform action after every checkout
+	hooks = {
+		post_checkout = function()
+			vim.cmd("TSUpdate")
+		end,
+	},
 })
 
-add('stevearc/oil.nvim')
-add('folke/which-key.nvim')
-add('catppuccin/nvim')
-add('ellisonleao/gruvbox.nvim' )
+add("stevearc/oil.nvim")
+add("folke/which-key.nvim")
+add("ellisonleao/gruvbox.nvim")
 
 -- Mason
-add('williamboman/mason.nvim')
+add("williamboman/mason.nvim")
 
+-- Conform
+add("stevearc/conform.nvim")
+
+-- Nvim lint (lint)
+add("mfussenegger/nvim-lint")
 
 local now, later = MiniDeps.now, MiniDeps.later
 
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd[[colorscheme gruvbox]]
+vim.cmd([[colorscheme gruvbox]])
 
 -- Mini configuration
-require('mini.basics').setup()
-require('mini.starter').setup()
-require('mini.icons').setup()
+require("mini.basics").setup()
+require("mini.starter").setup()
+require("mini.icons").setup()
 
 -- Mini configuration that can be configured later
-later(function ()
-	require('mini.surround').setup()
-	require('mini.statusline').setup()
-	require('mini.tabline').setup()
-	require('mini.jump').setup()
+later(function()
+	require("mini.surround").setup()
+	require("mini.statusline").setup()
+	require("mini.tabline").setup()
+	require("mini.jump").setup()
 end)
 
 -- Plugin configuration
-now(function ()
-	require('nvim-treesitter.configs').setup({
-	  ensure_installed = { 'lua', 'vimdoc' },
-	  highlight = { enable = true },
-	})
-end
-)
-
-later(function ()
-	require('oil').setup()
-end)
-
-later(function ()
-	require('which-key').setup()
-end)
-
-now(function ()
-	require('mason').setup({
-	  ensure_installed = {
-		  "gopls",
-		  "lua-language-server",
-		  "rust-analyzer",
-		  "stylua",
-	  }
+now(function()
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = { "lua", "vimdoc" },
+		highlight = { enable = true },
 	})
 end)
 
+later(function()
+	require("oil").setup()
+end)
 
+later(function()
+	require("which-key").setup()
+end)
+
+now(function()
+	require("mason").setup({
+		ensure_installed = {
+			"gopls",
+			"lua-language-server",
+			"rust-analyzer",
+			"stylua",
+		},
+	})
+end)
+
+now(function()
+	local lspconfig_defaults = require("lspconfig").util.default_config
+	lspconfig_defaults.capabilities =
+		vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+	-- This is where you enable features that only work
+	-- if there is a language server active in the file
+	vim.api.nvim_create_autocmd("LspAttach", {
+		desc = "LSP actions",
+		callback = function(event)
+			local opts = { buffer = event.buf }
+
+			vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+			vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+			vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+			vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+			vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+			vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+			vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+			vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+			vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+			vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+		end,
+	})
+end)
+
+now(function()
+	local lspconfig_defaults = require("lspconfig").util.default_config
+	lspconfig_defaults.capabilities =
+		vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
+end)
+
+-- LSP setup
+now(function()
+	local lspconfig = require("lspconfig")
+	require("lspconfig").lua_ls.setup({})
+end)
+
+-- CMP
+now(function()
+	local cmp = require("cmp")
+
+	cmp.setup({
+		sources = {
+			{ name = "nvim_lsp" },
+			{ name = "buffer" },
+		},
+		snippet = {
+			expand = function(args)
+				-- You need Neovim v0.10 to use vim.snippet
+				vim.snippet.expand(args.body)
+			end,
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<C-e>"] = cmp.mapping.abort(),
+			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		}),
+	})
+end)
+
+later(function()
+	require("conform").setup({
+		formatters_by_ft = {
+			lua = { "stylua" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+	})
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*",
+		callback = function(args)
+			require("conform").format({ bufnr = args.buf })
+		end,
+	})
+end)
+
+later(function()
+	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+		callback = function()
+			-- try_lint without arguments runs the linters defined in `linters_by_ft`
+			-- for the current filetype
+			require("lint").try_lint()
+
+			-- You can call `try_lint` with a linter name or a list of names to always
+			-- run specific linters, independent of the `linters_by_ft` configuration
+			-- require("lint").try_lint("cspell")
+		end,
+	})
+end)
